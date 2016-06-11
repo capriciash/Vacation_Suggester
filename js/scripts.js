abc229b79dd7070bea77296617937ebf5af5cdf4
Ashley Riehl <ashleyriehl@MacBook-Air-2.local>
2016-06-10 23:34:10 -0700

    add and format all of possible vacation locations in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,29 @@
 $(document).ready(function() {
   $("form#survey").submit(function() {
+    $("#instructions").remove();
     event.preventDefault();
     var nameInput = $("input#name").val();
     var ageInput = parseInt($("#age").val());
-    var personalityInput= $("#personality").val();
-    var childrenInput = $("#children").val();
-    var pastimeInput = $("#pastime").val();
-    var weatherInput = $("#weather").val();
-    var landscapeInput = $("#landscape").val();
-    var drinkInput = $("#drink").val();
+    var personalityInput= $("input:radio[name=personality]:checked").val();
+    var childrenInput = $("input:radio[name=children]:checked").val();
+    var pastimeInput = $("input:radio[name=pastime]:checked").val();
+    var weatherInput = $("input:radio[name=weather]:checked").val();
+    var landscapeInput = $("input:radio[name=landscape]:checked").val();
+    var drinkInput = $("input:radio[name=drink]:checked").val();
 
-    $("#name").text(nameInput);
+    $(".name").text(nameInput);
 
     if (ageInput <= 16 || childrenInput === "yes") {
       $("#disneyland").show();
-      }
+    } else if (drinkInput === "tea") {
+      $("#london").show();
+    }
+
+
 
 // Hide all of the top materials
 //add picture to each destination
+//get the name to be inserted into response
 
 //all of the locations still needing to be assigned
 //    $("#cancun").show();
