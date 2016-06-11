fa986def1076609521d27cd6ae5e1bb81d59337f
Ashley Riehl <ashleyriehl@MacBook-Air-2.local>
2016-06-11 00:04:27 -0700

    finish all of the if statements and add picture formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,39 @@
 $(document).ready(function() {
   $("form#survey").submit(function() {
-    $("#instructions").remove();
+    $("#instructions").hide();
+    $("#survey").hide();
+
     event.preventDefault();
     var nameInput = $("input#name").val();
     var ageInput = parseInt($("#age").val());
     var personalityInput= $("input:radio[name=personality]:checked").val();
     var childrenInput = $("input:radio[name=children]:checked").val();
-    var pastimeInput = $("input:radio[name=pastime]:checked").val();
+    var outdoorInput = $("input:radio[name=outdoor]:checked").val();
     var weatherInput = $("input:radio[name=weather]:checked").val();
     var landscapeInput = $("input:radio[name=landscape]:checked").val();
     var drinkInput = $("input:radio[name=drink]:checked").val();
 
     $(".name").text(nameInput);
 
-    if (ageInput <= 16 || childrenInput === "yes") {
+    if (ageInput <= 16) {
       $("#disneyland").show();
-    } else if (drinkInput === "tea") {
+    } else if (drinkInput === "tea" && landscapeInput === "city") {
       $("#london").show();
+    } else if (outdoorInput === "skiing" || weatherInput === "snowy" || landscapeInput === "mountains") {
+      $("#switzerland").show();
+    } else if (landscapeInput === "desert" || weatherInput === "hot") {
+      $("#egypt").show();
+    } else if (drinkInput === "wine" || personalityInput === "artsy") {
+      $("#florence").show();
+    } else if (landscapeInput === "trees" || outdoorInput === "hiking") {
+      $("#yellowstone").show();
+    } else if (drinkInput === "frozen" && landscapeInput === "ocean" || weatherInput === "tropical") {
+      $("#cancun").show();
+    } else if (childrenInput === "yes") {
+      $("#disneyland").show();
+    } else {
+      $("#cancun").show();
     }
-
-
-
-// Hide all of the top materials
-//add picture to each destination
-//get the name to be inserted into response
-
-//all of the locations still needing to be assigned
-//    $("#cancun").show();
-//    $("#yellowstone").show();
-//    $("#egypt").show();
-//    $("#london").show();
-//    $("#florence").show();
-//    $("#switzerland").show();
-
 
   });
 });
